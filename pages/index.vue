c156acda28a06a27dce9b63c67376cf59feec9a7
wuj <wuj@yindou.com>
2018-05-08 18:45:24 +0800

    git commit -m "first commit"

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <h1 class="title">
+      hello boy~
+    </h1>
+    <nuxt-link class="button" to="/about">
+      ABOUT
+    </nuxt-link>
+  </section>
+</template>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
